40b15277de9d722835dc594f94fc13701eb68ce6
jimohsegun1 <jimohsegunj@gmail.com>
2024-08-28 16:54:39 +0100

    navbar

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primeColor: "#262626",
+        lightText: "#6D6D6D",
+      },
+      boxShadow: {
+        testShadow: "0px 0px 54px -13px rgba(0,0,0,0.7)",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
